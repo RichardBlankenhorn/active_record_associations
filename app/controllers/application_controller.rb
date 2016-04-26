a712b1c526e4d30a687e6e6c61afe3e16b6d8f4f
Richard Blankenhorn <rsbnq6@yahoo.com>
2016-04-25 22:42:40 -0600

    Adding Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Pundit
+
   protect_from_forgery with: :exception
 
   helper_method :current_user
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   private
+
+  def user_not_authorized
+    flash[:alert] = 'Access not allowed'
+    redirect_to(request.referrer || root_path)
+  end
 
   def current_user
     @current_user ||= User.find(session[:id]) if session[:id]
